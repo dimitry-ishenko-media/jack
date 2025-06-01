5c5f94985cd30cf96d8a3bd98d06576881c71b25
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2025-05-31 21:10:40 -0400

    Add sidetone example

---
+++ example/sidetone.cpp
@@ -0,0 +1,57 @@
+#include <algorithm>
+#include <csignal>
+#include <iostream>
+#include <jack++.hpp>
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+try
+{
+    cout << "Connecting to JACK server" << endl;
+
+    jack::client_options options{
+        .server_name = "jack",
+        .inputs = {"capture"},
+        .outputs = {"playback"},
+    };
+    jack::client client{"sidetone", options};
+
+    client.on_data([&](const jack::input_ports& inputs, const jack::output_ports& outputs, std::size_t size)
+    {
+        auto in = inputs[0].buffer(size);
+        auto out = outputs[0].buffer(size);
+        std::copy(in.begin(), in.end(), out.begin());
+    });
+
+    auto&& capture = client.input("capture");
+    for (auto&& output : client.find_ports({}, jack::physical, jack::out)) client.connect(output, capture);
+
+    auto&& playback = client.output("playback");
+    for (auto&& input : client.find_ports({}, jack::physical, jack::in)) client.connect(playback, input);
+
+    cout << endl;
+    cout << "SYSTEM PORTS" << endl;
+    for (auto&& name : client.find_ports({}, jack::physical, jack::in | jack::out)) cout << "  " << name << endl;
+
+    cout << endl;
+    cout << "CLIENT PORTS" << endl;
+    for (auto&& input : client.inputs()) cout << "  " << input.name() << endl;
+    for (auto&& output : client.outputs()) cout << "  " << output.name() << endl;
+    cout << endl;
+
+    std::signal(SIGINT, [](int signal) {
+        cout << "Received signal " << signal << ", exiting." << endl;
+    });
+
+    cout << "Press CTRL+C to exit." << endl;
+    pause();
+
+    return 0;
+}
+catch (const jack::error& e)
+{
+    std::cerr << e.what() << std::endl;
+    return 1;
+}
